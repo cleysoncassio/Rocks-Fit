506a87f5dd1eea10b7be9f3d41015c7a61a74a07
cleyson <cleysoncassio@gmail.com>
2024-08-19 16:45:05 -0300

    inserido coverallss

--- blog/base/test_home.py
+++ blog/base/test_home.py
@@ -1,4 +1,19 @@
+import os
+import sys
+import django
 from django.test import Client
+from django.conf import settings
+from django.test.utils import get_runner
+
+
 def test_status_code(client:Client):
     resp=client.get('/')
     assert resp.status_code == 200
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'sitio.settings'
+django.setup()
+
+TestRunner = get_runner(settings)
+test_runner = TestRunner()
+failures = test_runner.run_tests(['blog'])
+sys.exit(failures)
